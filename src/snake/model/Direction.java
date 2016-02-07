6ffb5f82045173942abfc057a3325f00ecd69b92
Yevhen <eugeny.berkunsky@gmail.com>
2016-02-08 00:16:50 +0200

    for some reason

--- src/snake/model/Direction.java
+++ src/snake/model/Direction.java
@@ -4,10 +4,10 @@ package snake.model;
  * Created by Yevhen on 05.02.2016.
  */
 public interface Direction {
-    Direction UP = new North();
-    Direction RIGHT = new East();
-    Direction LEFT = new West();
-    Direction DOWN = new South();
+    Direction UP = element -> new SnakeElement(element.getX(), element.getY()-1);
+    Direction RIGHT = element -> new SnakeElement(element.getX()+1, element.getY());
+    Direction LEFT = element -> new SnakeElement(element.getX()-1, element.getY());
+    Direction DOWN = element -> new SnakeElement(element.getX(), element.getY()+1);
 
     SnakeElement next(SnakeElement element);
 }
